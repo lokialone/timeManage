f876709c0da4556cfc6bb45ebea663c71da154df
lokialone <648875862@qq.com>
2016-11-14 01:03:14 +0800

    some try

--- js/background.js
+++ js/background.js
@@ -1,16 +1,39 @@
-import 'whatwg-fetch'
-
-let googleUrl = "http://www.google.cn"
-
-
-
-
-function checkStatus(){
-  
-    fetch(googleUrl).then((res)=>{
-        chrome.browserAction.setIcon({path: 'images/offline.png'});
-        setTimeout(checkStatus, 5000);
-    })
+function httpRequest(url, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(true);
+        }
+    }
+    xhr.onerror = function(){
+        callback(false);
+    }
+    xhr.send();
 }
 
-checkStatus();
+// function checkStatus(){
+//     httpRequest('http://www.google.com/', function(status){
+//         chrome.browserAction.setIcon({path: 'images/'+(status?'online.png':'offline.png')});
+//         setTimeout(checkStatus, 5000);
+//     });
+// }
+chrome.browserAction.setBadgeBackgroundColor({color: '#0000FF'});
+chrome.browserAction.setBadgeText({text: 'hii'});
+// checkStatus();
+
+function timeCount() {
+
+
+
+}
+
+function checkDomain() {
+
+}
+
+chrome.tabs.query({
+    active: true
+}, function(tabArray){
+    console.log(tabArray);
+});
